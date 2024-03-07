1e9c858b14e4eb139bf94ed6889b444ab325cfa3
Michael <michael@bcpp.com>
2024-03-07 05:50:42 -0500

    assign in_addr_any to interface ip address in echo demo

--- src/executable/echo_server_demo/main.cpp
+++ src/executable/echo_server_demo/main.cpp
@@ -15,9 +15,9 @@ int main
     using namespace bcpp::network::literals;
     using namespace std::chrono;
 
-    // this example will not define a network interface.  Therefore use 'any' interface.
-    echo_server echoServer({}, 3000_port);
-    echo_client echoClient({}, {echoServer.get_ip_address(), 3000_port});
+    // this example will use 'any' interface.
+    echo_server echoServer(bcpp::network::network_interface_configuration{.ipAddress_ = bcpp::network::in_addr_any}, 3000_port);
+    echo_client echoClient(bcpp::network::network_interface_configuration{.ipAddress_ = bcpp::network::in_addr_any}, {echoServer.get_ip_address(), 3000_port});
 
     for (auto i = 0; i < 100; ++i)
         while (!echoClient.send(fmt::format("message {}\n", i)));
